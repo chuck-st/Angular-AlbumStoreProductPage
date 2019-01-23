ab4f358639a4413c8338ac91fe9cc19ebec05ab7
Jonathan Williams <jwilliams@medrio.com>
2019-01-22 17:59:09 -0800

    another fix to part4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -10,6 +10,6 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   public getAlbum(id: number): Observable<Album> {
-    return this._http.get(this._albumUrl).map((response) => response.json() as Album);
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 }
